772850868c8fcbab7bf9e11166d7d4811c56baaf
cwd0204 <cwd0204@users.noreply.github.com>
2022-08-19 15:10:35 +0800

    d
    d

--- sample.py
+++ sample.py
@@ -9,13 +9,13 @@ translate_and_compose(input_file, 'sample_en_cn_both.srt', 'en', 'zh-CN')
 
 
 # Translate the subtitle into Chinese, save only Chinese subtitle to the output srt file
-translate_and_compose(input_file, 'sample_cn_only.srt', 'en', 'zh-CN', both=False)
+# translate_and_compose(input_file, 'sample_cn_only.srt', 'en', 'zh-CN', both=False)
 
 # Translate the subtitle into German, save both English and German to the output srt file
 # In German language, each words separated by space, so space=True
-translate_and_compose(input_file, 'sample_en_de_both.srt', 'en', 'de', space=True)
+# translate_and_compose(input_file, 'sample_en_de_both.srt', 'en', 'de', space=True)
 
 # Translate the subtitle into Japanese, save both English and Japanese to the output srt file
 # In Japanese(Chinese, Korean), words are characters which are NOT separated by space, so space=False (default)
-translate_and_compose(input_file, 'sample_en_ja_both.srt', 'en', 'ja')
+# translate_and_compose(input_file, 'sample_en_ja_both.srt', 'en', 'ja')
 
